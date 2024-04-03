2d0a06833b4dd14561620621113b5638a4e72f30
jingjingccc <ccc.jing.jing@gmail.com>
2024-04-03 16:16:15 +0800

    add launch, cofig, maps for different scenario

--- cartographer_ros/configuration_files/gazebo_sim_localization.lua
+++ cartographer_ros/configuration_files/gazebo_sim_localization.lua
@@ -17,6 +17,7 @@ include "gazebo_sim.lua"
 TRAJECTORY_BUILDER.pure_localization_trimmer = {
   max_submaps_to_keep = 3,
 }
--- POSE_GRAPH.optimize_every_n_nodes = 100
+POSE_GRAPH.optimize_every_n_nodes = 5
+
 
 return options
--- cartographer_ros/configuration_files/pigeon_localization.lua
+++ cartographer_ros/configuration_files/pigeon_localization.lua
@@ -19,12 +19,12 @@ options = {
   map_builder = MAP_BUILDER,
   trajectory_builder = TRAJECTORY_BUILDER,
   map_frame = "map",
-  tracking_frame = "base_footprint",
+  tracking_frame = "unilidar_imu",
   published_frame = "odom",
   odom_frame = "odom",
   provide_odom_frame = false,
   publish_frame_projected_to_2d = false, 
-  use_pose_extrapolator = false,
+  use_pose_extrapolator = true,
   use_odometry = true,
   use_nav_sat = false,
   use_landmarks = false,
@@ -32,10 +32,10 @@ options = {
   num_multi_echo_laser_scans = 0,
   num_subdivisions_per_laser_scan = 1,
   num_point_clouds = 1,
-  lookup_transform_timeout_sec = 0.2,
+  lookup_transform_timeout_sec = 0.5,
   submap_publish_period_sec = 0.3,
-  pose_publish_period_sec = 5e-3,
-  trajectory_publish_period_sec = 30e-3,
+  pose_publish_period_sec = 5e-2,
+  trajectory_publish_period_sec = 30e-2,
   rangefinder_sampling_ratio = 1.,
   odometry_sampling_ratio = 1.,
   fixed_frame_pose_sampling_ratio = 1.,
@@ -44,15 +44,33 @@ options = {
 }
 
 TRAJECTORY_BUILDER_3D.num_accumulated_range_data = 1
+TRAJECTORY_BUILDER_3D.voxel_filter_size = 0.1
+TRAJECTORY_BUILDER_3D.ceres_scan_matcher.ceres_solver_options.max_num_iterations = 15
+TRAJECTORY_BUILDER_3D.max_range = 20
+TRAJECTORY_BUILDER_3D.submaps.high_resolution = 0.1
+TRAJECTORY_BUILDER_3D.submaps.low_resolution = 0.4
+TRAJECTORY_BUILDER_3D.submaps.num_range_data = 200
+-- TRAJECTORY_BUILDER_3D.high_resolution_adaptive_voxel_filter.min_num_points = 60
+TRAJECTORY_BUILDER_3D.high_resolution_adaptive_voxel_filter.max_range = 10
+-- TRAJECTORY_BUILDER_3D.high_resolution_adaptive_voxel_filter.max_length = 5
+-- TRAJECTORY_BUILDER_3D.low_resolution_adaptive_voxel_filter.min_num_points = 100
+TRAJECTORY_BUILDER_3D.low_resolution_adaptive_voxel_filter.max_range = 20
+-- TRAJECTORY_BUILDER_3D.low_resolution_adaptive_voxel_filter.max_length = 7
 
 MAP_BUILDER.use_trajectory_builder_3d = true
-MAP_BUILDER.num_background_threads = 5
+MAP_BUILDER.num_background_threads = 2
 POSE_GRAPH.optimization_problem.huber_scale = 5e2
-POSE_GRAPH.optimize_every_n_nodes = 0
--- POSE_GRAPH.optimize_every_n_nodes = 320
-POSE_GRAPH.constraint_builder.sampling_ratio = 0.03
-POSE_GRAPH.optimization_problem.ceres_solver_options.max_num_iterations = 10
+-- POSE_GRAPH.optimize_every_n_nodes = 0
+POSE_GRAPH.optimize_every_n_nodes = 3
+POSE_GRAPH.constraint_builder.sampling_ratio = 0.3
+POSE_GRAPH.optimization_problem.ceres_solver_options.max_num_iterations = 50
 POSE_GRAPH.constraint_builder.min_score = 0.62
 POSE_GRAPH.constraint_builder.global_localization_min_score = 0.66
+POSE_GRAPH.constraint_builder.max_constraint_distance = 20
+POSE_GRAPH.global_sampling_ratio = 0.3
+
+TRAJECTORY_BUILDER.pure_localization_trimmer = {
+  max_submaps_to_keep = 3,
+}
 
 return options
